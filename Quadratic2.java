75bf02df095ec10e3d3eb5f5f94415c755075fea
yeswanth8555 <134755925+yaswanthyashu855@users.noreply.github.com>
2025-07-21 12:12:08 +0530

    Create Quadratic2.java

---
+++ Quadratic2.java
@@ -0,0 +1,44 @@
+//This code is written using objects(Basic)
+
+import static java.lang.System.out;
+import java.util.Scanner;
+import static java.lang.Math.sqrt;
+
+public class Main {
+ 
+  public static void main(String[] args) { 
+     Scanner input = new Scanner(System.in);
+    
+     out.print("Enter the coefficent of x^2 : ");
+     float a = input.nextFloat();
+     out.print("Enter the coefficent of x : ");
+     float b = input.nextFloat();
+     out.print("Enter the constant: ");
+     float c = input.nextFloat();
+
+     Quadratic example = new Quadratic();
+     example.roots(a, b, c);
+  }
+
+}
+
+class Quadratic {
+  private float root1, root2;
+  public float discriminant, real, imaginary;
+  public void  roots(float a, float b, float c) {
+    discriminant = b*b - 4*a*c;
+  
+   if(discriminant >= 0 ) {
+     root1 = (float ) (-b + sqrt(discriminant)) / (2*a); 
+     root2 = (float) (-b - sqrt(discriminant)) / (2*a); 
+     out.println("Roots are " + root1 + " and " + root2 + ".");
+   }
+   else {
+    real = -b / (2*a);
+    imaginary = (float) sqrt(-discriminant) / (2*a);
+    out.println("Roots are " + real + " + " + imaginary + "i , and " + real + "-" + imaginary + "i.");  
+    }
+
+  }
+  
+}
